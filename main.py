932f1ef5d04b5ba52d5a1ab3403dc92e9e20f6e8
Kamil Boksa <kamil.boksa@gmail.comkamil.boksa@gmail.comexithelpgit congig --global user.namegit config --global user.email kamil.boksa@gmail.com>
2017-10-26 15:37:01 +0200

    Addprinting user interface

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from todo_controller import *
 
 
+def import_ui():
+    ui = []
+    with open('user_interface.txt') as ui_file:
+        for line in ui_file:
+            ui.append(line)
+    return "".join(ui)
+
+
 def main():
-    pass
+    print(import_ui())
 
 
 if __name__ == "__main__":
