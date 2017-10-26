997cf14b3189255cbe7e13f822a886ae9c9bde87
Kamil Boksa <kamil.boksa@gmail.comkamil.boksa@gmail.comexithelpgit congig --global user.namegit config --global user.email kamil.boksa@gmail.com>
2017-10-26 16:07:37 +0200

    Add todo_list methods

---
+++ todo_list.py
@@ -0,0 +1,22 @@
+from todo_item import *
+
+
+class ToDoList:
+
+    def __init__(self):
+        self.todo_items = []
+
+    def add_item(self, item):
+        self.todo_items.append(item)
+
+    def delete_item(self, item_name):
+        try:
+            self.todo_items.remove(item_name)
+        except ValueError:
+            pass
+
+    def __str__(self):
+        display_list = []
+        for index, item in enumerate(self.todo_items):
+            display_list.append(str(index)+str(item))
+        return "".join(display_list)
