e753c679610f03fcbf02d2cbc7b65a921520fbd2
Kamil Boksa <kamil.boksa@gmail.comkamil.boksa@gmail.comexithelpgit congig --global user.namegit config --global user.email kamil.boksa@gmail.com>
2017-10-28 18:03:14 +0200

    Delegated inputs to main module

--- main.py
+++ main.py
@@ -9,9 +9,88 @@ def import_ui():
     return "".join(ui)
 
 
+def create_task_name():
+    while True:
+        task_name = input("Enter task name (max 20 chars): ")
+        if len(task_name) > 20:
+            print("Too long task name!")
+            continue
+        elif len(task_name) == 0:
+            print("Task name cannot be empty!")
+            continue
+        return task_name
+
+
+def create_task_description():
+    while True:
+        task_description = input("Enter task description (max 150 chars):")
+        if len(task_description) > 150:
+            print("Too long task description!")
+            continue
+        return task_description
+
+
+def handle_ui_choice(tasks_list):
+    while True:
+        try:
+            choice = input("User choice: ")
+            if choice == "1":
+                task_name = create_task_name()
+                task_description = create_task_description()
+                task = create_task(task_name, task_description)
+                add_task_to_list(tasks_list, task)
+            elif choice == "2":
+                    task_name = input("Enter name of task you want to edit: ")
+                    if check_if_task_in_list(tasks_list, task_name):
+                        decision = input("""
+                                    [1] Edit name.
+                                    [2] Edit description.
+                                    : """)
+                        if decision == "1":
+                            while True:
+                                new_name = input("Enter task new name: ")
+                                if len(new_name) > 20:
+                                    print("Too long task name!")
+                                    continue
+                                modify_task_name(tasks_list, task_name, new_name)
+                                break
+                        elif decision == "2":
+                            while True:
+                                new_description = input("Enter task new description: ")
+                                if len(new_description) > 150:
+                                    print("Too long task description!")
+                                    continue
+                                modify_task_description(tasks_list, task_name, new_description)
+                                break
+                        else:
+                            print("Wrong  decision input!")
+            elif choice == "3":
+                task_name = input("Enter name of task you want to delete: ")
+                delete_task_from_list(tasks_list, task_name)
+            elif choice == "4":
+                task_name = input("Enter name of task you want to mark: ")
+                mark_task(tasks_list, task_name)
+            elif choice == "5":
+                task_name = input("Enter name of task you want to unmark: ")
+                unmark_task(tasks_list, task_name)
+            elif choice == "6":
+                print(tasks_list)
+            elif choice == "7":
+                task_name = input("Enter name of task you want to display: ")
+                display_specific_task(tasks_list, task_name)
+            elif choice == "0":
+                tasks_list.save_items_to_file('saved_tasks.txt')
+                exit()
+            else:
+                print("Invalid input!")
+        except ValueError:
+            print("No task with that name found!")
+
+
 def main():
+    tasks_list = initialize_tasks_list()
     print(import_ui())
-    handle_ui_choice()
+    handle_ui_choice(tasks_list)
 
 
 if __name__ == "__main__":
--- todo_controller.py
+++ todo_controller.py
@@ -2,145 +2,73 @@ from todo_item import *
 from todo_list import *
 
 
-def create_task():
-    creating_task = True
-    while creating_task:
-        task_name = input("Enter task name (max 20 chars): ")
-        if len(task_name) > 20:
-            print("Too long task name!")
-            continue
-        elif len(task_name) == 0:
-            print("Task name cannot be empty!")
-            continue
-        break
-    while creating_task:
-        task_description = input("Enter task description (max 150 chars):")
-        if len(task_description) > 150:
-            print("Too long task description!")
-            continue
-        task = ToDoItem(task_name, task_description)
-        return task
+def create_task(task_name, task_description):
+    task = ToDoItem(task_name, task_description)
+    return task
 
 
 def add_task_to_list(todo_items, task):
     todo_items.add_item(task)
 
 
-def delete_task_from_list(todo_items):
-    try:
-        task_name = input("Enter name of task you want to delete: ")
-        for task in todo_items.todo_items:
-            if task_name == task.name:
-                todo_items.delete_item(task)
-                break
-        else:
-            raise ValueError
-    except ValueError:
-        print("No task with that name found!")
+def check_if_task_in_list(todo_items, task_name):
+    for task in todo_items.todo_items:
+        if task_name == task.name:
+            return True
+    raise ValueError
 
 
-def modify_task_name(task):
-    while True:
-        new_name = input("Enter task new name: ")
-        if len(new_name) > 20:
-            print("Too long task name!")
-            continue
-        task.change_task_name(new_name)
-        break
+def delete_task_from_list(todo_items, task_name):
+    for task in todo_items.todo_items:
+        if task_name == task.name:
+            todo_items.delete_item(task)
+            break
+    else:
+        raise ValueError
 
 
-def modify_task_description(task):
-    while True:
-        new_description = input("Enter task new description: ")
-        if len(new_description) > 150:
-            print("Too long task description!")
-            continue
-        task.change_task_description(new_description)
-        break
+def modify_task_name(todo_items, task_name, new_name):
+    for task in todo_items.todo_items:
+        if task_name == task.name:
+            task.change_task_name(new_name)
+            break
 
 
-def modify_task(todo_items):
-    task_name = input("Enter name of task you want to edit: ")
-    try:
-        for task in todo_items.todo_items:
-            if task_name == task.name:
-                choice = input("""
-                [1] Edit name.
-                [2] Edit description.
-                : """)
-                if choice == "1":
-                    modify_task_name(task)
-                    break
-                elif choice == "2":
-                    modify_task_description(task)
-                    break
-        else:
-            raise ValueError
-    except ValueError:
-        print("No task with that name found!")
+def modify_task_description(todo_items, task_name, new_description):
+    for task in todo_items.todo_items:
+        if task_name == task.name:
+            task.change_task_description(new_description)
+            break
 
 
-def mark_task(todo_items):
-    try:
-        task_name = input("Enter name of task you want to mark: ")
-        for task in todo_items.todo_items:
-            if task_name == task.name:
-                task.mark_item()
-                break
-        else:
-            raise ValueError
-    except ValueError:
-        print("No task with that name found!")
+def mark_task(todo_items, task_name):
+    for task in todo_items.todo_items:
+        if task_name == task.name:
+            task.mark_item()
+            break
+    else:
+        raise ValueError
 
 
-def unmark_task(todo_items):
-    try:
-        task_name = input("Enter name of task you want to unmark: ")
-        for task in todo_items.todo_items:
-            if task_name == task.name:
-                task.unmark_item()
-                break
-        else:
-            raise ValueError
-    except ValueError:
-        print("No task with that name found!")
+def unmark_task(todo_items, task_name):
+    for task in todo_items.todo_items:
+        if task_name == task.name:
+            task.unmark_item()
+            break
+    else:
+        raise ValueError
 
 
-def display_specific_task(todo_items):
-    try:
-        task_name = input("Enter name of task you want to display: ")
-        for index, task in enumerate(todo_items.todo_items):
-            if task_name == task.name:
-                print("ID:" + str(index) + " " + str(task))
-                break
-        else:
-            raise ValueError
-    except ValueError:
-        print("No task with that name found!")
+def display_specific_task(todo_items, task_name):
+    for index, task in enumerate(todo_items.todo_items, 1):
+        if task_name == task.name:
+            print("ID:" + str(index) + " " + str(task))
+            break
+    else:
+        raise ValueError
 
 
-def handle_ui_choice():
+def initialize_tasks_list():
     todo_items = ToDoList()
     todo_items.load_items_from_file('saved_tasks.txt')
-
-    while True:
-        choice = input("User choice: ")
-        if choice == "1":
-            add_task_to_list(todo_items, create_task())
-        elif choice == "2":
-            modify_task(todo_items)
-        elif choice == "3":
-            delete_task_from_list(todo_items)
-        elif choice == "4":
-            mark_task(todo_items)
-        elif choice == "5":
-            unmark_task(todo_items)
-        elif choice == "6":
-            print(todo_items)
-        elif choice == "7":
-            display_specific_task(todo_items)
-        elif choice == "0":
-            todo_items.save_items_to_file('saved_tasks.txt')
-            exit()
-        else:
-            print("Invalid input!")
+    return todo_items
